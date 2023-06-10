0a078d3a4149ad67ede5d48f1d03429c09432124
nothing-available <smtkur31@gmail.com>
2023-06-10 23:20:10 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: RecomendedFoodDetail(),
+      home: const RecomendedFoodDetail(),
     );
   }
 }
