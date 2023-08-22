0d170b7ad0442b200cfad2f8526401dd6f782e7c
nothing-available <smtkur31@gmail.com>
2023-08-22 12:09:52 +0530

    update

---
+++ lib/data/api/api_client.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+
+class ApiClient extends GetConnect implements GetxService {
+  late String token;
+  final String appBaseUrl;
+  late Map<String, String> _mainHeader;
+
+  ApiClient({required this.appBaseUrl}) {
+    baseUrl = appBaseUrl;
+    timeout = Duration(seconds: 30);
+    _mainHeader = {
+      'Content-type':'application/json; charset=UTF-8',
+      'Authorazation':'Bearer $token',
+    };
+  }
+}
---
+++ lib/helper/dependencies.dart
@@ -0,0 +1,3 @@
+Future<void> init()async {
+  
+}
