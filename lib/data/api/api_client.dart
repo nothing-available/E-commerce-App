c3ba8b0c04b7d0659b2d5f6c529b21742dcdba24
nothing-available <smtkur31@gmail.com>
2023-08-23 22:54:38 +0530

    update

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -1,3 +1,5 @@
+// This ApiClient class is designed to manage API requests using the GetX package. It initializes the base URL and headers needed for API requests, with the ability to include an authentication token. It extends the capabilities of GetConnect for making HTTP requests while leveraging the benefits of GetX's state management
+
 import 'package:get/get.dart';
 
 class ApiClient extends GetConnect implements GetxService {
@@ -11,7 +13,7 @@ class ApiClient extends GetConnect implements GetxService {
     _mainHeader = {
       'Content-type':'application/json; charset=UTF-8',
       'Authorazation':'Bearer $token',
-      
+
     };
   }
 }
